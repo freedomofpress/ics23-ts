694e62889bb50f867020ecdcf9736e3e33f30bdc
giulio <66009328+lsd-cat@users.noreply.github.com>
2025-12-02 23:05:06 +0100

    Disable non null assertion warning

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,7 @@ export default [
     rules: {
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
-      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-non-null-assertion": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
